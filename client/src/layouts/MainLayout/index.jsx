4c369a9209941fb6a80e7f7b1735da4250c51ddd
DuyTranNhat <duytn1053@gmail.com>
2026-05-17 15:29:58 +0700

    feat: add Header component

--- client/src/layouts/MainLayout/index.jsx
+++ client/src/layouts/MainLayout/index.jsx
@@ -1,9 +1,11 @@
 
 import { Fragment } from "react";
+import Header from "~/components/Header";
 
 const MainLayout = ({ children }) => {
     return (
         <Fragment>
+            <Header />
             {children}
         </Fragment>
     );
--- client/src/layouts/SignInLayout/index.jsx
+++ client/src/layouts/SignInLayout/index.jsx
@@ -1,9 +1,11 @@
 
 import { Fragment } from "react";
+import Header from "~/components/Header";
 
 const SignInLayout = ({ children }) => {
     return (
         <Fragment>
+            <Header />
             {children}
         </Fragment>
     );
